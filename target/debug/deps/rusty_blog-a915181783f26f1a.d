c5649b2b791d80c5a12ff4bba509c5ea0262acc1
thibgil <zaideby@gmail.com>
2024-04-20 13:33:11 +0200

    init cargo rust project

---
+++ target/debug/deps/rusty_blog-a915181783f26f1a.d
@@ -0,0 +1,5 @@
+/home/imp/Bureau/rusty-blog/target/debug/deps/rusty_blog-a915181783f26f1a: src/main.rs
+
+/home/imp/Bureau/rusty-blog/target/debug/deps/rusty_blog-a915181783f26f1a.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rusty-blog.d
@@ -0,0 +1,1 @@
+/home/imp/Bureau/rusty-blog/target/debug/rusty-blog: /home/imp/Bureau/rusty-blog/src/main.rs
